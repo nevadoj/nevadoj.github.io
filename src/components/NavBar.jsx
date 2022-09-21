24da246627ce4ad86810b26a06bfbc3de10bfe08
Joseph <josephnevado12@gmail.com>
2022-09-21 01:00:41 -0700

    Starting navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>
-      Navbar
+    <div className='flex justify-between items-center w-full h-20 px-4 text-black bg-slate-200'>
+        <div>
+            <h1 className='text-3xl'>Testing</h1>
+        </div>
     </div>
   )
 }
