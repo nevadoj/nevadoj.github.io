65cb483fdc0da6edc7608ffa7c5c335ad3c1c76f
Joseph <josephnevado12@gmail.com>
2022-10-07 17:02:19 -0700

    adjusted navbar appearance

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div name="home" className="h-screen w-full bg-[#495046]">
+      <div>
+        <div>
+            <h2>Testing</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 
 export default function NavBar() {
+  
+  const links = [
+    {
+      id: 1,
+      link: 'projects'
+    },
+    {
+      id: 2,
+      link: 'about'
+    },
+  ]
+
+
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-black bg-slate-200'>
-        <div>
-            <h1 className='text-3xl'>Testing</h1>
-        </div>
+    // <div className='flex justify-end items-center w-full h-20 px-4 text-3xl text-white bg-[#495046]'>
+    //     {/* <div>
+    //         <h1 className='text-3xl font-medium tracking-tight'>Spotter</h1>
+    //     </div> */}
+
+    //     {/* change ul classname if want navbar to show on mobile */}
+    //     <div className='mx-50'>
+    //       <ul className="flex"> 
+    //         {links.map(({ id, link }) => (
+    //           <li key={id} className="px-3 cursor-pointer font-normal">
+    //             {link}
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    // </div>
+
+    <div className='flex justify-end text-2xl text-white bg-[#495046]'>
+      <ul className="flex mx-20 my-10"> 
+        {links.map(({ id, link }) => (
+          <li key={id} className="px-4 cursor-pointer font-light">
+            {link}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
