4cc097021ff3b430269b7b97f29ed3ff9337a7cc
Joseph <josephnevado12@gmail.com>
2022-10-09 02:49:50 -0700

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,11 @@ export default function Home() {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row">
         <div className="pb-96">
              <h2 className="text-4xl font-medium py-20">Hello 👋</h2>
-             <p className="text-4xl">My name is <span className="font-medium">Joseph</span>.</p>
-             <p className="text-4xl">I'm a <span className="font-medium">Computer Science</span> undergraduate student</p>
-             <p className='text-4xl'>at <span className="font-medium">Simon Fraser University</span>.</p>
+             <div className='font-light'>
+              <p className="text-4xl">My name is <span className="font-medium">Joseph</span>.</p>
+              <p className="text-4xl">I'm a <span className="font-medium">Computer Science</span> undergraduate student</p>
+              <p className='text-4xl'>at <span className="font-medium">Simon Fraser University</span>.</p>
+             </div>
         </div>
       </div>
     </div>
