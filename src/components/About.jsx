eefb4ad0d6c82f4f4907d31012f4e1829ce31904
Joseph Nevado <josephnevado12@gmail.com>
2022-10-11 17:02:06 -0700

    add svg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,11 @@ export default function About() {
         <div className="max-w-screen-lg mx-auto">
             <div className="font-light max-w-4xl mx-24">
                 <p className="text-4xl py-10">
-                    I'm interested in leveraging technology for our daily lifestyles. I'm also interested in design and how it evokes emotion. Currently exploring <span className="font-medium">iOS development</span>.
+                    I'm passionate about all things technology. I'm also interested in design and how it evokes emotion. Currently exploring <span className="font-medium">iOS development</span>.
                 </p>
 
                 <p className="text-4xl py-10">
-                    Outside of programming, I enjoy playing guitar and building mechanical keyboards.
+                    Outside of programming, I enjoy playing guitar, listening to music and building mechanical keyboards.
                 </p>
             </div>
         </div>
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Social() {
   return (
-    <div>Social</div>
+    <div name="social"className="h-screen w-full bg-[#495046] text-white">
+      <div className="max-w-screen-lg mx-auto">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48" strokeWidth={0.2} stroke="currentColor" className="w-16 h-16">
+          <path d="M24 4C16.26 4 10 10.26 10 18C10 28.5 24 44 24 44C24 44 38 28.5 38 18C38 10.26 31.74 4 24 4ZM14 18C14 12.48 18.48 8 24 8C29.52 8 34 12.48 34 18C34 23.76 28.24 32.38 24 37.76C19.84 32.42 14 23.7 14 18Z" fill="white"/>
+          <path d="M24 23C26.7614 23 29 20.7614 29 18C29 15.2386 26.7614 13 24 13C21.2386 13 19 15.2386 19 18C19 20.7614 21.2386 23 24 23Z" fill="white"/>
+          </svg>
+      </div>
+    </div>
   )
 }
