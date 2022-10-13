059b0b7f3e28a9e0e879e4ac0ae516d908d8292a
Joseph <josephnevado12@gmail.com>
2022-10-12 21:21:32 -0700

    start of projects section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home"
 import About from "./components/About"
+import Projects from "./components/Projects"
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <Projects />
       <About />
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div name="projects" className="h-screen w-full bg-[#495046] text-white">
+      <div className="max-w-screen-lg mx-auto flex flex-col h-full">
+        <div className="font-light max-w-4xl mx-24 my-24">
+            <p className="text-4xl font-medium">Spotter</p>
+            <div className="flex justify-start py-2 text-xl font-medium text-[#A6A6A6]">
+                <p className="pr-4">Swift</p>
+                <p>SwiftUI</p>
+            </div>
+            <p className='text-3xl py-8'>App that does blank</p>
+        </div>
+      </div>
+    </div>
+  )
+}
