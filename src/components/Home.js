a9ccc55bd78d5f1544fd9c33cc67448a9ec0591f
Joseph <josephnevado12@gmail.com>
2022-11-10 00:51:28 -0800

    add animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div name="home" className="h-screen w-full bg-altbg text-white">
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row animate-fade-in-up">
         <div className="-translate-y-36">
              <div className='font-light max-w-4xl mx-24'>
               <h2 className="text-4xl font-medium py-20">Hello 👋</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,55 @@ module.exports = {
       colors: {
         'mainbg': '#495046',
         'altbg': '#2E3342',
-      }
+      },
+      keyframes: {
+        'fade-in-down': {
+            '0%': {
+                opacity: '0',
+                transform: 'translateY(-10px)'
+            },
+            '100%': {
+                opacity: '1',
+                transform: 'translateY(0)'
+            },
+        },
+        'fade-out-down': {
+            'from': {
+                opacity: '1',
+                transform: 'translateY(0px)'
+            },
+            'to': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+        },
+        'fade-in-up': {
+            '0%': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+            '100%': {
+                opacity: '1',
+                transform: 'translateY(0)'
+            },
+        },
+        'fade-out-up': {
+            'from': {
+                opacity: '1',
+                transform: 'translateY(0px)'
+            },
+            'to': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+        }
+    },
+    animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-out-down': 'fade-out-down 0.5s ease-out',
+        'fade-in-up': 'fade-in-up 1.5s ease-out',
+        'fade-out-up': 'fade-out-up 0.5s ease-out'
+    },
     },
   },
   plugins: [],
