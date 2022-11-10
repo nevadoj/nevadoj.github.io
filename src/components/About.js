8c80636b6f2ec5625ca21d6056d349cb40b5cfd2
Joseph <josephnevado12@gmail.com>
2022-11-10 00:04:50 -0800

    temp site

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default function About() {
   return (
     <div name="about" className="w-full h-screen bg-[#495046] text-white sm:py-2">
         <div className="max-w-screen-lg mx-auto bg-[#495046]">
-            <div className="font-light max-w-4xl mx-24 pt-32">
+            <div className="font-light max-w-4xl mx-24 pt-24">
                 <p className="text-4xl py-10">
                     I'm interested in leveraging technology for our daily lifestyles. I'm also interested in design and how it evokes emotion. Currently exploring <span className="font-medium">iOS development</span>.
                 </p>
