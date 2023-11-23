3cbc06569c665f6b17c42aaddbf37b9f9df94ce6
Joseph Nevado <josephnevado12@gmail.com>
2023-11-23 00:51:17 -0800

    add breakpoints for icon

--- src/app/components/content/Hero.tsx
+++ src/app/components/content/Hero.tsx
@@ -2,7 +2,9 @@ export default function Hero(){
   return (
     <>
       <div className="flex flex-col items-center sm:items-start w-full">
-        <img src="/assets/images/icon.png" alt="website icon" width={60} height={60} className="mx-12 hover:cursor-pointer"/>
+        <div className="flex justify-center lg:justify-start w-full">
+          <img src="/assets/images/icon.png" alt="website icon" width={60} height={60} className="mx-12 hover:cursor-pointer"/>
+        </div>
 
         <div className="flex flex-col justify-center items-center w-full pt-24 min-w-5/6 sm:flex-row sm:pt-36">
           
