31648633f8b40d07c8e93e623e850ee71cd47dbe
Joseph Nevado <josephnevado12@gmail.com>
2023-11-26 15:51:25 -0800

    adjust spacing

--- src/app/components/content/Hero.tsx
+++ src/app/components/content/Hero.tsx
@@ -6,13 +6,12 @@ export default function Hero(){
           <img src="/assets/images/icon.png" alt="website icon" width={60} height={60} className="mx-12 hover:cursor-pointer"/>
         </div>
 
-        <div className="flex flex-col justify-center items-center w-full pt-24 min-w-5/6 sm:flex-row sm:pt-36">
-          
+        <div className="flex flex-col justify-center items-center pt-24 w-11/12 min-w-5/6 lg:flex-row lg:pt-36">
           <p className="text-2xl sm:px-20">Hello 👋</p>
           
           <div className="flex flex-col sm:px-20">
-            <p className="pt-6 pb-2 text-center sm:text-left">My name is <span className="text-text-dark">Joseph</span>.</p>
-            <p className="text-center sm:text-left">I'm a <span className="text-text-dark">Computer Science</span> undergraduate student at <span className="text-text-dark">Simon Fraser University</span>.</p>
+            <p className="pt-6 pb-2 text-center lg:text-left">My name is <span className="text-text-dark">Joseph</span>.</p>
+            <p className="text-center lg:text-left">I'm a <span className="text-text-dark">Computer Science</span> undergraduate student at <span className="text-text-dark">Simon Fraser University</span>.</p>
           </div>
 
         </div>
