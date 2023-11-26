664bf814129370c063f4e106ecfeb98af37619b0
Joseph Nevado <josephnevado12@gmail.com>
2023-11-26 13:01:37 -0800

    adjust

--- src/app/components/elements/ProjectHeading.tsx
+++ src/app/components/elements/ProjectHeading.tsx
@@ -2,7 +2,7 @@ interface ProjectHeadingProps{
     title: string;
 }
 
-export default function ProjectHeading({title}){
+const ProjectHeading: React.FC<ProjectHeadingProps> = ({title}) => {
     return (
         <>
             <p className="pl-4 pr-2 text-text-dark text-xl transition ease-in-out group-hover:pointer group-hover:underline">{title}</p>
@@ -10,3 +10,5 @@ export default function ProjectHeading({title}){
         </>
     )
 }
+
+export default ProjectHeading
