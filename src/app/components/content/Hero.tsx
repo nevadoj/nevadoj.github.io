ab83ef0a988047bf8447677415effc985c7e8260
Joseph Nevado <josephnevado12@gmail.com>
2023-11-28 14:50:59 -0800

    move header from hero component

--- src/app/components/content/Hero.tsx
+++ src/app/components/content/Hero.tsx
@@ -1,22 +1,18 @@
+import Link from "next/link"
 import Container from "../layout/Container"
 
 export default function Hero(){
   return (
     <>
-      <div>
-          <div className="flex justify-center lg:justify-start w-full">
-              <img src="/assets/images/icon.png" alt="website icon" width={60} height={60} className="mx-12 hover:cursor-pointer pb-24 lg:pb-36"/>
+      <Container>
+          <div className="flex justify-center items-start lg:justify-start">
+              <p className="text-2xl">Hello 👋</p>
           </div>
-          <Container>
-              <div className="flex justify-center items-start lg:justify-start">
-                  <p className="text-2xl">Hello 👋</p>
-              </div>
-              <div className="flex flex-col justify-center items-center lg:justify-start lg:items-start">
-                  <p className="pb-2 text-center lg:text-left lg:pt-0">My name is <span className="text-text-dark">Joseph</span>.</p>
-                  <p className="text-center lg:text-left">I'm a <span className="text-text-dark">Computer Science</span> undergraduate student at <span className="text-text-dark">Simon Fraser University</span>.</p>
-              </div>
-          </Container>
-      </div>
+          <div className="flex flex-col justify-center items-center lg:justify-start lg:items-start">
+              <p className="pb-2 text-center lg:text-left lg:pt-0">My name is <span className="text-text-dark">Joseph</span>.</p>
+              <p className="text-center lg:text-left">I'm a <span className="text-text-dark">Computer Science</span> undergraduate student at <span className="text-text-dark">Simon Fraser University</span>.</p>
+          </div>
+      </Container>
     </>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Link from 'next/link'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -10,7 +11,12 @@ export default function Main({ children, }: { children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>
-        <main className='flex flex-col gap-36 lg:gap-64 text-text-regular px-20 py-20'>
+        <main className='flex flex-col gap-36 lg:gap-52 text-text-regular px-20 py-20'>
+            <div className="flex justify-center lg:justify-start w-full">
+              <Link href="/">
+                <img src="/assets/images/icon.png" alt="website icon" width={60} height={60} className="mx-12 hover:cursor-pointer"/>
+              </Link>
+            </div>
             {children}
         </main>
       </body>
