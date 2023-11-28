f6badc0141185bf6acf47a1ea96377605a6684ba
Joseph Nevado <josephnevado12@gmail.com>
2023-11-28 14:51:04 -0800

    spacing

--- src/app/components/elements/ProjectEntry.tsx
+++ src/app/components/elements/ProjectEntry.tsx
@@ -23,14 +23,14 @@ const ProjectEntry: React.FC<ProjectProps> = ({title, description, source, langu
                         <p className="text-text-dark text-lg w-full">{title}</p>
                     )
                 }
-                <div className="flex flex-row w-full pt-2">
+                <div className="flex flex-row w-full pt-1">
                     {languages.map((language, idx) => <p className="pr-2 last-of-type:pr-0" key={idx}>{language}</p>)}
                 </div>
             </div>
 
             <div className="flex flex-col justify-start items-start lg:pt-8 lg:pb-16">
                 <p className="pt-1">{description}</p>
-                <img src={preview ? preview : "/assets/images/placeholder.png"} alt="project placeholder picture" className="flex object-cover pt-6 max-w-md max-h-m lg:pt-3 lg:max-w-xl lg:max-h-xl"/>
+                {/* <img src={preview ? preview : "/assets/images/placeholder.png"} alt="project placeholder picture" className="flex object-cover pt-6 max-w-md max-h-m lg:pt-3 lg:max-w-xl lg:max-h-xl"/> */}
             </div>
         </Container>
     )
