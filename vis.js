4ed1870d91fc884c3d5b7b044a0975730d59708a
Joseph Nevado <josephnevado12@gmail.com>
2025-09-30 13:23:33 -0700

    add vis chart

--- vis.js
+++ vis.js
@@ -1,12 +1,14 @@
 console.log("vis.js loaded");
 
 const typingData = [0, 0, 0, 0, 0, 0, 0, 1, 14, 67, 88, 97, 158, 221, 215, 102, 32, 4, 1];
+
 const svg = document.getElementById("chart");
 const chartWidth = svg.getAttribute("width");
 const chartHeight = svg.getAttribute("height");
 const chartLabelsX = document.getElementById("chart-label-x");
-
 const barWidth = chartWidth / typingData.length;
+
+const infobox = document.getElementById("infobox");
 
 typingData.forEach((value, i) => {
     const barHeight = value;
@@ -21,7 +23,25 @@ typingData.forEach((value, i) => {
     rect.setAttribute("height", barHeight);
     svg.appendChild(rect);
 
-    // Create text label
+    // Interaction
+    rect.addEventListener("mouseover", () => {
+        rect.setAttribute("fill", "orange");
+        infobox.style.display = "block";
+        const range = "WPM Range: " + (i*10) + '-' + ((i * 10) + 9);
+        infobox.textContent = `Tests completed: ${value}\n${range}`;
+    });
+
+    rect.addEventListener("mousemove", (e) => {
+        infobox.style.left = e.pageX + 10 + "px";
+        infobox.style.top = e.pageY - 20 + "px";
+    });
+
+    rect.addEventListener("mouseout", () => {
+        rect.setAttribute("fill", "steelblue");
+        infobox.style.display = "none";
+    });
+
+    // Create x-axis labels
     const label = document.createElement("p");
     label.textContent = (i*10) + '-' + ((i * 10) + 9); // labels 1–20
     label.style.width = (barWidth) + "px";
