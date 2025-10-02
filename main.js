eb220e04957c6296c3a04302c24bdbb11241d4a2
Joseph Nevado <josephnevado12@gmail.com>
2025-10-01 19:13:07 -0700

    update main js provider

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 async function getUserLocation() {
-    const url = "http://ip-api.com/json";
+    const url = "https://ipinfo.io/json";
     try {
         const response = await fetch(url);
         const result = await response.json();
         console.log(result);
 
         const userLocationText = document.getElementById("visitor");
-        userLocationText.textContent = "Last visitor from: " + result.city + ", " + result.country;
+        userLocationText.textContent = "Latest visitor from: " + result.city + ", " + result.region;
     }
     catch (error) {
         console.log(error);
